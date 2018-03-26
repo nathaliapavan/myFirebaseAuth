b1e7d4f28efa48fc2da72b11315dffe3e096b39f
Nathalia Pavan <nath@icozweb.com>
2018-03-25 21:05:29 -0300

    initial commit

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyA-FUy9NygvX-KY2uj9-tvggGoOBcFiO8w",
+    authDomain: "myfirebaseauth-84c27.firebaseapp.com",
+    databaseURL: "https://myfirebaseauth-84c27.firebaseio.com",
+    projectId: "myfirebaseauth-84c27",
+    storageBucket: "",
+    messagingSenderId: "869032502407"
+};
---
+++ src/pages/home/home.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+@IonicPage()
+@Component({
+	selector: 'page-home',
+	templateUrl: 'home.html',
+})
+export class HomePage {
+
+	constructor(private afAuth: AngularFireAuth, private toast: ToastController,
+		public navCtrl: NavController, public navParams: NavParams) {
+	}
+
+	ionViewDidLoad() {
+		this.afAuth.authState.subscribe(data => {
+			if(data && data.email && data.uid) {
+				this.toast.create({
+					message: `Welcome to APP_NAME, ${data.email}`,
+					duration: 3000
+				}).present();
+			}else {
+				this.toast.create({
+					message: `Could not find auth details`,
+					duration: 3000
+				}).present();
+			}
+		});
+	}
+}
